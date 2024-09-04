130f0692ec63bef4e44b517dec248aec7357a3db
hwkkk <376703830@qq.com>
2024-09-04 12:31:20 +0800

    product-service

---
+++ src/main/java/top/yun/productservice/Controller/ProductController.java
@@ -0,0 +1,21 @@
+package top.yun.productservice.Controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+/**
+ * @author Administrator
+ */
+@RestController
+public class ProductController {
+
+    @GetMapping("/product")
+    public String getProduct(@RequestParam String productId) {
+      return "Product: " + productId;
+    }
+}
