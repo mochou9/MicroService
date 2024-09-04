972176da3bbae48a401bf85b2edf2334eba27d07
hwkkk <376703830@qq.com>
2024-09-04 12:29:13 +0800

    user-service

---
+++ src/main/java/top/yun/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package top.yun.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/top/yun/userservice/entity/User.java
@@ -0,0 +1,8 @@
+package top.yun.userservice.entity;
+
+/**
+ * @Author yun
+ * @Date: 2024/09/04/ 上午10:59
+ * @description
+ */
+public record User(String username,String email){}
